3629e96e5d15dc95621d797e7d0fc55618e6901a
Дмитрий Вязников <d.vyaznikov@icloud.com>
2022-10-17 21:17:10 +0300

    ADD send notifications command

--- commands_groups/owner/owner.py
+++ commands_groups/owner/owner.py
@@ -1,9 +1,10 @@
 import discord
 from discord import app_commands
 
-from additions.all_data import config
+from additions.all_data import config, aco_members
 from additions.checkers import owner_checker
 from classes.discord_classes import CreateTicketView
+from additions.embeds import unpaid_successes
 
 
 @app_commands.guild_only()
@@ -28,3 +29,19 @@ class Owner(app_commands.Group):
     async def create_ticket_menu(self, interaction: discord.Interaction):
         await interaction.channel.send(content="Take your ACO", view=CreateTicketView(config.closed_category_id))
         await interaction.response.send_message("Menu created")
+
+    @app_commands.command(name="send-notifications", description="OWNER COMMAND sends payment notifications for users")
+    async def send_notifications(self, interaction: discord.Interaction):
+        data_to_send = ""
+        for member in aco_members:
+            content = f"<@{member.id}>\nFriendly reminder to pay for checkouts."
+            if any(member.payments[key]["unpaid_amount"] != 0 for key in member.payments):
+                try:
+                    await interaction.client.get_user(member.id).send(content=content, embed=unpaid_successes(member))
+                except discord.errors.Forbidden:
+                    if member.ticket_id is not None:
+                        await interaction.client.get_channel(member.ticket_id).send(content=content,
+                                                                                    embed=unpaid_successes(member))
+                    else:
+                        data_to_send += f"<@{member.id}>\n"
+        await interaction.response.send_message(f"Can't reach this members:\n{data_to_send}\n")
