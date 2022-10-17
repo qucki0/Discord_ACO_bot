fe7aa968e0cf9875bef47d2065298f2b43f2c160
Дмитрий Вязников <d.vyaznikov@icloud.com>
2022-10-17 15:06:55 +0300

    UPDATE mint request logic

--- classes/discord_classes.py
+++ classes/discord_classes.py
@@ -3,6 +3,8 @@ import discord
 from additions import embeds
 from classes.classes import ACOMember
 from functions.blockchain import submit_transaction, get_transaction_hash_from_string
+from functions.members import check_admin
+from functions.mints import add_mint_to_mints_list
 
 
 class SubmitTransactionView(discord.ui.View):
@@ -42,11 +44,61 @@ class SubmitTransactionView(discord.ui.View):
 
 
 class SubmitTransactionModal(discord.ui.Modal, title='Submit transaction'):
-    tx_hash = discord.ui.TextInput(label='Transaction hash or solscan link', style=discord.TextStyle.short,
-                                   placeholder="Transaction", max_length=150, required=True)
+    tx_hash = discord.ui.TextInput(label='Transaction hash or solscan link', placeholder="Transaction", max_length=150)
     interaction = None
 
     async def on_submit(self, interaction: discord.Interaction) -> None:
         self.interaction = interaction
         self.tx_hash = str(self.tx_hash)
+        self.stop()
+
+
+class RequestMintView(discord.ui.View):
+    def __init__(self, original_interaction: discord.Interaction, release_id: str) -> None:
+        self.original_interaction = original_interaction
+        self.release_id = release_id
+        super().__init__()
+
+    @discord.ui.button(label="Accept", style=discord.ButtonStyle.green)
+    async def accept_response(self, accept_interaction: discord.Interaction, button: discord.ui.Button) -> None:
+        if not check_admin(accept_interaction.user.id):
+            await accept_interaction.response.send_message(
+                "You do not have enough permissions to do perform this operation.", ephemeral=True)
+            return
+        add_mint_modal = AddMintModal()
+        await accept_interaction.response.send_modal(add_mint_modal)
+        await add_mint_modal.wait()
+        await add_mint_to_mints_list(add_mint_modal.interaction, add_mint_modal.release_name, add_mint_modal.link,
+                                     add_mint_modal.timestamp, add_mint_modal.wallets_limit)
+        await self.original_interaction.edit_original_response(
+            content=f":green_circle:Request for `{self.release_id}` accepted",
+            view=None)
+
+    @discord.ui.button(label="Reject", style=discord.ButtonStyle.red)
+    async def reject_response(self, reject_interaction: discord.Interaction, button: discord.ui.Button) -> None:
+        if not check_admin(reject_interaction.user.id):
+            await reject_interaction.response.send_message(
+                "You do not have enough permissions to do perform this operation.", ephemeral=True)
+            return
+        await self.original_interaction.edit_original_response(
+            content=f":red_circle:Request for `{self.release_id}` rejected", view=None)
+
+
+class AddMintModal(discord.ui.Modal, title='Create mint'):
+    release_name = discord.ui.TextInput(label='Release name')
+    wallets_limit = discord.ui.TextInput(label='Wallets limit')
+    link = discord.ui.TextInput(label='Link', required=False)
+    timestamp = discord.ui.TextInput(label='Timestamp', required=False)
+    interaction = None
+
+    async def on_submit(self, interaction: discord.Interaction) -> None:
+        self.release_name = str(self.release_name)
+        if str(self.link) == "":
+            self.link = None
+        if str(self.timestamp) == "":
+            self.timestamp = None
+        else:
+            self.timestamp = int(str(self.timestamp))
+        self.wallets_limit = int(str(self.wallets_limit))
+        self.interaction = interaction
         self.stop()
--- commands_groups/member/mints.py
+++ commands_groups/member/mints.py
@@ -2,8 +2,8 @@ import discord
 from discord import app_commands
 
 from additions.all_data import actual_mints, config
-from functions.members import check_admin
-from functions.mints import add_mint_to_mints_list, check_mint_exist
+from functions.mints import check_mint_exist
+from classes.discord_classes import RequestMintView
 
 
 @app_commands.guild_only()
@@ -19,45 +19,16 @@ class Mints(app_commands.Group):
                 embeds=embeds_to_send[10 * i:min(10 * (i + 1), len(embeds_to_send))])
 
     @app_commands.command(name="request", description="Create request to add mint")
-    @app_commands.describe(release_id="Name of release you want to request",
-                           link="Link for mint page if you got",
-                           mint_timestamp="You can use https://www.epochconverter.com/ to get timestamp in seconds")
+    @app_commands.describe(release_id="Name of release you want to request")
     @discord.app_commands.rename(release_id="release_name")
-    async def request_mint(self, interaction: discord.Interaction, release_id: str, link: str = None,
-                           mint_timestamp: int = None) -> None:
+    async def request_mint(self, interaction: discord.Interaction, release_id: str) -> None:
         admins_to_ping = ""
         if check_mint_exist(release_id):
             await interaction.response.send_message(f"`{release_id}` already exist!", ephemeral=True)
             return
         for admin_id in config.admins:
             admins_to_ping += "<@" + str(admin_id) + "> "
-        accept_button = discord.ui.Button(label="Accept", style=discord.ButtonStyle.green)
-        reject_button = discord.ui.Button(label="Reject", style=discord.ButtonStyle.red)
-
-        async def accept_response(accept_interaction: discord.Interaction) -> None:
-            if not check_admin(accept_interaction.user.id):
-                await accept_interaction.response.send_message(
-                    "You do not have enough permissions to do perform this operation.", ephemeral=True)
-                return
-            await add_mint_to_mints_list(accept_interaction, release_id, link, mint_timestamp)
-            await interaction.delete_original_response()
-            await accept_interaction.client.get_channel(accept_interaction.channel_id).send(
-                f":green_circle:Request for `{release_id}` accepted")
-
-        async def reject_response(reject_interaction: discord.Interaction) -> None:
-            if not check_admin(reject_interaction.user.id):
-                await reject_interaction.response.send_message(
-                    "You do not have enough permissions to do perform this operation.", ephemeral=True)
-                return
-            await interaction.delete_original_response()
-            await reject_interaction.client.get_channel(reject_interaction.channel_id).send(
-                ":red_circle:Request Rejected")
-
-        accept_button.callback = accept_response
-        reject_button.callback = reject_response
-        view = discord.ui.View(timeout=None)
-        view.add_item(accept_button)
-        view.add_item(reject_button)
+        view = RequestMintView(interaction, release_id)
 
         await interaction.response.send_message(
-            f"{admins_to_ping}, please add `{release_id}`, link:`{link}`, time: `{mint_timestamp}`", view=view)
+            f"{admins_to_ping}, please add `{release_id}`", view=view)
