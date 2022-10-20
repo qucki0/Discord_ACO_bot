c9101aa11a8709aced8088ff0955d9cda91927fe
Дмитрий Вязников <d.vyaznikov@icloud.com>
2022-10-20 21:30:10 +0300

    ADD auto notifications

--- classes/discord_client.py
+++ classes/discord_client.py
@@ -1,12 +1,13 @@
 import discord
 from discord.ext import commands
 
+from additions.all_data import config
+from classes.discord_classes import CreateTicketView, TicketView
 from commands_groups.admin_group import AdminMints, AdminWallets, Admin, AdminPayments
 from commands_groups.member_group import Mints, Wallets, Payments, ask_help, WalletManager
 from commands_groups.owner_group import Owner, OwnerCheckers, OwnerStatistic
 from functions.files import auto_backup
-from classes.discord_classes import CreateTicketView, TicketView
-from additions.all_data import config
+from functions.paymnets import auto_send_notifications
 
 
 class DiscordClient(commands.Bot):
@@ -24,4 +25,5 @@ class DiscordClient(commands.Bot):
         self.add_view(TicketView(config.closed_category_id))
         await self.tree.sync()
         self.loop.create_task(auto_backup(self))
+        self.loop.create_task(auto_send_notifications(self))
         print(f'{self.user} has connected to Discord!')
--- functions/paymnets.py
+++ functions/paymnets.py
@@ -1,3 +1,10 @@
+import asyncio
+import time
+
+import discord
+
+from additions.all_data import aco_members, config
+from additions.embeds import unpaid_successes
 from classes.classes import ACOMember, Drop
 
 
@@ -9,3 +16,31 @@ def add_checkouts(member: ACOMember, mint: Drop, amount_to_add: int) -> None:
         member.payments[mint.id]["amount_of_checkouts"] += amount_to_add
         member.payments[mint.id]["unpaid_amount"] += amount_to_add
     mint.checkouts += amount_to_add
+
+
+async def send_notifications(client: discord.Client) -> None:
+    response = ""
+    for member in aco_members:
+        content = f"<@{member.id}>\nFriendly reminder to pay for checkouts."
+        if any(member.payments[key]["unpaid_amount"] != 0 for key in member.payments):
+            try:
+                await client.get_user(member.id).send(content=content, embed=unpaid_successes(member))
+            except discord.errors.Forbidden:
+                if member.ticket_id is not None:
+                    await client.get_channel(member.ticket_id).send(content=content,
+                                                                    embed=unpaid_successes(member))
+                else:
+                    response += f"<@{member.id}>\n"
+    await client.get_channel(config.notifications_channel_id).send(f"Can't reach this members:\n{response}\n")
+
+
+async def auto_send_notifications(client: discord.Client) -> None:
+    timestamp_to_send = 1666429200
+    week = 7 * 24 * 60 * 60
+    while timestamp_to_send < int(time.time()):
+        timestamp_to_send += week
+
+    while True:
+        time_to_wait = timestamp_to_send - int(time.time())
+        await asyncio.sleep(time_to_wait)
+        await send_notifications(client)
