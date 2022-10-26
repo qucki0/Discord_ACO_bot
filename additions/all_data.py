26174abbc48a0a1fad44d323ed6a769f7d926cf7
Дмитрий Вязников <d.vyaznikov@icloud.com>
2022-10-26 06:56:44 +0300

    DELETE sql_client.start()

--- additions/all_data.py
+++ additions/all_data.py
@@ -6,5 +6,4 @@ from classes.sql_client import SqlClient
 config = Config.parse_file("config.json")
 sql_client = SqlClient(config.sql_data.user, config.sql_data.password, config.sql_data.db_name, config.sql_data.host,
                        config.sql_data.port)
-sql_client.start()
 solana_client = Client(config.rpc_link)
