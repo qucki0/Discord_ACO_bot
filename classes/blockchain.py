311d3bc7a551a3e55a02fdcc27d83eaa4e361ab6
Дмитрий Вязников <d.vyaznikov@icloud.com>
2022-10-26 05:30:09 +0300

    REFACTOR classes using pydantic.BaseModel

--- classes/blockchain.py
+++ classes/blockchain.py
@@ -1,27 +1,8 @@
-from classes.classes import ACOMember
+from pydantic import BaseModel
 
 
-class Transaction:
-    def __init__(self, member: ACOMember = None, transaction_hash: str = None, amount: float = None,
-                 timestamp: int = None, input_dict: dict = None) -> None:
-        if input_dict is None:
-            self.member_id = member.id
-            self.member_name = member.name
-            self.hash = transaction_hash
-            self.amount = amount
-            self.timestamp = timestamp
-        else:
-            self.member_id = int(input_dict["member_id"])
-            self.member_name = input_dict["member_name"]
-            self.hash = input_dict["hash"]
-            self.amount = float(input_dict["amount"])
-            self.timestamp = int(input_dict["timestamp"])
-
-    def get_as_dict(self) -> dict:
-        data = {"member_id": self.member_id,
-                "member_name": self.member_name,
-                "hash": self.hash,
-                "amount": self.amount,
-                "timestamp": self.timestamp
-                }
-        return data
+class Transaction(BaseModel):
+    member_id: int
+    hash: str
+    amount: float
+    timestamp: int
--- classes/classes.py
+++ classes/classes.py
@@ -1,73 +1,35 @@
-from dataclasses import dataclass
-
 import discord
 from pydantic import BaseModel
 
 
-class Drop:
-    def __init__(self, mint_id: str = None, link: str = None, timestamp: int = None, wallets_limit: int = None,
-                 input_dict: dict = None) -> None:
-        if input_dict is None:
-            self.id = mint_id.strip()
-            self.wallets_limit = wallets_limit
-            self.timestamp = timestamp
-            self.link = link
-            self.wallets = {}
-            self.checkouts = 0
-        else:
-            self.id = input_dict["id"]
-            self.wallets_limit = int(input_dict["wallets_limit"])
-            self.timestamp = input_dict["timestamp"]
-            self.link = input_dict["link"]
-            self.wallets = {int(key): set(input_dict["wallets"][key]) for key in input_dict["wallets"]}
-            self.checkouts = int(input_dict["checkouts"])
-
-    def get_as_dict(self) -> dict:
-        data = {"id": self.id,
-                "wallets_limit": self.wallets_limit,
-                "timestamp": self.timestamp,
-                "link": self.link,
-                "wallets": {key: list(self.wallets[key]) for key in self.wallets},
-                "checkouts": self.checkouts}
-        return data
-
-    def get_wallets_by_id(self, member_id: int) -> set[str]:
-        if member_id in self.wallets:
-            return self.wallets[member_id]
+class Mint(BaseModel):
+    name: str
+    wallets_limit: int
+    timestamp: int = None
+    link: str = None
+    checkouts: int = 0
 
     def get_as_embed(self) -> discord.Embed:
         from additions import embeds
         return embeds.mint_data(self)
 
 
-class ACOMember:
-    def __init__(self, member: discord.Member = None, input_dict: dict = None) -> None:
-        if input_dict is None:
-            self.id = member.id
-            self.name = member.name
-            self.mints = {}
-            self.payments: dict[str, dict[str, int]] = {}
-            self.ticket_id: int | None = None
-        else:
-            self.id = int(input_dict["id"])
-            self.name = input_dict["name"]
-            self.mints = input_dict["mints"]
-            self.payments = input_dict["payments"]
-            if "ticket_id" in input_dict:
-                self.ticket_id = input_dict["ticket_id"]
-                if self.ticket_id is not None:
-                    self.ticket_id = int(self.ticket_id)
-            else:
-                self.ticket_id = None
+class ACOMember(BaseModel):
+    id: int
+    name: str
+    ticket_id: int = None
 
-    def get_as_dict(self) -> dict:
-        data = {"id": self.id,
-                "name": self.name,
-                "mints": self.mints,
-                "payments": self.payments,
-                "ticket_id": self.ticket_id
-                }
-        return data
+
+class Wallet(BaseModel):
+    private_key: str
+    mint_id: int
+    member_id: int
+
+
+class Payment(BaseModel):
+    mint_id: int
+    member_id: int
+    amount_of_checkouts: int
 
 
 class SqlData(BaseModel):
@@ -91,8 +53,3 @@ class Config(BaseModel):
     closed_category_id: int
     notifications_channel_id: int
     sql_data: SqlData
-
-
-@dataclass
-class BackupData:
-    files_to_backup: list[tuple[list, str]]
