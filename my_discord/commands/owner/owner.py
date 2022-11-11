70ff8ba2d43e23549d08b05d65cd106d7b5ef0cb
Дмитрий Вязников <d.vyaznikov@icloud.com>
2022-11-11 06:03:50 +0300

    REFACTOR code refactoring

--- my_discord/commands/owner/owner.py
+++ my_discord/commands/owner/owner.py
@@ -26,26 +26,29 @@ class Owner(app_commands.Group):
     @app_commands.check(owner_checker)
     @app_commands.describe(user="User that will lose admin role")
     async def delete_admin(self, interaction: discord.Interaction, user: discord.Member) -> None:
+        await interaction.response.defer()
         if user.id not in config.admins:
-            await interaction.response.send_message(f"{user.name} is not admin", ephemeral=True)
+            await interaction.followup.send(f"{user.name} is not admin", ephemeral=True)
         config.admins.remove(user.id)
         logger.info(f"{interaction.user.id}, {interaction.user.name} deleted Admin role from {user.id}, {user.name}")
-        await interaction.response.send_message(f"Deleted {user.name} from admins list", ephemeral=True)
+        await interaction.followup.send(f"Deleted {user.name} from admins list", ephemeral=True)
 
     @app_commands.command(name="create-ticket-menu", description="OWNER COMMAND creates ticket menu")
     @app_commands.check(owner_checker)
     async def create_ticket_menu(self, interaction: discord.Interaction):
+        await interaction.response.defer()
         await interaction.channel.send(embed=tickets_menu(), view=CreateTicketView(config.closed_category_id))
-        await interaction.response.send_message("Menu created", ephemeral=True)
+        await interaction.followup.send("Menu created", ephemeral=True)
 
     @app_commands.command(name="send-notifications", description="OWNER COMMAND sends payment notifications for users")
     @app_commands.check(owner_checker)
     async def send_notifications(self, interaction: discord.Interaction):
+        await interaction.response.defer()
         logger.info(f"{interaction.user.id}, {interaction.user.name} started payments notifications sending")
         await send_notifications(interaction.client)
-        await interaction.response.send_message(f"Notifications gone, check <#{config.notifications_channel_id}>")
+        await interaction.followup.send(f"Notifications gone, check <#{config.notifications_channel_id}>")
 
     async def on_error(self, interaction: discord.Interaction, error: app_commands.AppCommandError) -> None:
-        await interaction.response.send_message(
+        await interaction.followup.send(
             "An unexpected error occurred, try again. If that doesn't work, ping the admin")
         logger.exception(f"{interaction.user} {interaction.user.id} got error \n {error}")
